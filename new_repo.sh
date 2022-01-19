6848cde1c74bf6b8d11e911a343f7c66a2840b68
Albert <Asdn1143>
2022-01-19 19:45:09 +0100

    finished base version of script for new git repositories

--- new_repo.sh
+++ new_repo.sh
@@ -1,2 +1,20 @@
-#! bin/bash 
+#!/bin/bash 
+echo "Name of the repository:"
+read REPONAME
+
+echo "Creating boilerplate"
+mkdir $REPONAME
+cd $REPONAME
+touch Readme.md
+git init 
+git add -A
+git commit -m"initial commit" 
+hub create 
+git push --set-upstream origin master
+
+
+
+
+
+
 
