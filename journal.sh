0a012dd88d55ea2743df859b14935caa7b8781c6
Albert <Asdn1143>
2022-01-30 21:42:01 +0100

    added read command for cloud integration preparation

--- journal.sh
+++ journal.sh
@@ -12,7 +12,21 @@ filepath=$path/$file
 
 test -f $filepath || touch $filepath
 
+
+
 $editor $filepath
+
+
+sleep 1s
+echo "Save to cloud? (Y/n)"
+read -d  -s -n1 ; #Passes input keystroke to #REPLY
+clear
+
+
+
+
+
+
 
 
 
