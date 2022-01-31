45a4c3e9334d02104af4e6adbf1f909750799822
Albert <Asdn1143>
2022-01-31 02:28:21 +0100

    Added more functionality for cloud uploading

--- journal.sh
+++ journal.sh
@@ -1,25 +1,47 @@
 #! bin/bash
 
-clear
+cleanup(){ 
+    sleep 1s; clear
+}
 
+#Editable variables
 editor=vim;
 format=txt;
 path=~/Documents/JournalEntries
 
+clear
+
+#-----------------------#
 date=$(date +'%d.%m.%y');
 file=$date.$format;
 filepath=$path/$file
+#-----------------------#
 
 test -f $filepath || touch $filepath
 
-
-
 $editor $filepath
 
-
-sleep 1s
+while true
+do
 echo "Save to cloud? (Y/n)"
-read -d  -s -n1 ; #Passes input keystroke to #REPLY
+read -d  -s -n1 ; #Passes input keystroke to $REPLY
+
+clear
+
+    case $REPLY in
+
+        y)
+        echo "Uploaded to cloud"; break; cleanup ;;
+
+        n)
+        echo -n "Saving for later"; break; cleanup ;;
+
+        *)
+        echo -n "Wrong input"; cleanup ;;
+
+    esac
+done 
+
 clear
 
 
