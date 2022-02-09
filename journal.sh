218e02a05dc839f9aa72335d3c3cd783904cdd89
Albert <Asdn1143>
2022-02-09 16:37:52 +0100

    Uploading to Google drive using a bash script turned out to be unrealistic with my current competence

--- journal.sh
+++ journal.sh
@@ -21,29 +21,6 @@ test -f $filepath || touch $filepath
 
 $editor $filepath
 
-while true
-do
-echo "Save to cloud? (Y/n)"
-read -d  -s -n1 ; #Passes input keystroke to $REPLY
-
-clear
-
-    case $REPLY in
-
-        y)
-        echo "Uploaded to cloud"; cleanup ; break ;;
-
-        n)
-        echo -n "Saving for later"; cleanup ; break ;;
-
-        *)
-        echo -n "Wrong input"; cleanup ;;
-
-    esac
-done 
-
-clear
-
 
 
 
